d5a2044bbd2a592d2e73ee6ff797eb9170cb6d24
Andrew Nasef <androamazing@gmail.com>
2024-08-27 00:46:15 +0300

    fix(ux): when go back in intro it goes to homepage not exit the app as should be

--- lib/ui/screens/intro/intro_screen.dart
+++ lib/ui/screens/intro/intro_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:leyana/core/values.dart';
@@ -41,70 +42,75 @@ class _IntroScreenState extends State<IntroScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: SingleChildScrollView(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Image(image: AssetImage('assets/images/hello.png')),
-              const SizedBox(height: 8),
-              Text(
-                "اهلا بيك!",
-                style: Theme.of(context)
-                    .textTheme
-                    .titleLarge
-                    ?.copyWith(fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                "اهلا بيك في ابليكشن ليا انا او في الحقيقة هو ليك انت ده المكان اللي بتصلي وبتفتكر في انه الله عمل حاجات كتيير جميلة علشانك\nهنا بنشجعك انك تعيش الكلمة وتكون ليك انت 💖.\nبس قبل ما لازم ندخل البيانات اللي تحت دي!\nشكرا واتمنالك رحله جميلة 🚀",
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyMedium
-                    ?.copyWith(height: 1.35),
-              ),
-              const SizedBox(height: 32),
-              ConstrainedBox(
-                constraints: const BoxConstraints(maxWidth: 250),
-                child: TextField(
-                  controller: _nameField,
-                  decoration: InputDecoration(
-                    prefixIcon: const Icon(Icons.person),
-                    hintText: 'ادخل اسمك الاول',
-                    labelText: 'اسمك',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  ),
-                  // icon of name
+    return PopScope(
+      onPopInvoked: (didPop) => didPop ? exit(0) : null,
+      child: SafeArea(
+        child: Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: SingleChildScrollView(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Image(image: AssetImage('assets/images/hello.png')),
+                const SizedBox(height: 8),
+                Text(
+                  "اهلا بيك!",
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleLarge
+                      ?.copyWith(fontWeight: FontWeight.bold),
                 ),
-              ),
-              const SizedBox(height: 16),
-              SegmentedButton<bool>(
-                onSelectionChanged: (Set<bool> newSelection) {
-                  setState(() {
-                    _isMale = newSelection.first;
-                  });
-                },
-                showSelectedIcon: true,
-                segments: const <ButtonSegment<bool>>[
-                  ButtonSegment<bool>(
-                      value: true, label: Text('ولد'), icon: Icon(Icons.boy)),
-                  ButtonSegment<bool>(
-                      value: false, label: Text('بنت'), icon: Icon(Icons.girl)),
-                ],
-                selected: <bool>{_isMale},
-              ),
-              const SizedBox(height: 24),
-              FilledButton(
-                onPressed: onContinuePressed,
-                child: const Text("اتفضل"),
-              )
-            ],
-          )),
+                const SizedBox(height: 8),
+                Text(
+                  "اهلا بيك في ابليكشن ليا انا او في الحقيقة هو ليك انت ده المكان اللي بتصلي وبتفتكر في انه الله عمل حاجات كتيير جميلة علشانك\nهنا بنشجعك انك تعيش الكلمة وتكون ليك انت 💖.\nبس قبل ما لازم ندخل البيانات اللي تحت دي!\nشكرا واتمنالك رحله جميلة 🚀",
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyMedium
+                      ?.copyWith(height: 1.35),
+                ),
+                const SizedBox(height: 32),
+                ConstrainedBox(
+                  constraints: const BoxConstraints(maxWidth: 250),
+                  child: TextField(
+                    controller: _nameField,
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.person),
+                      hintText: 'ادخل اسمك الاول',
+                      labelText: 'اسمك',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    // icon of name
+                  ),
+                ),
+                const SizedBox(height: 16),
+                SegmentedButton<bool>(
+                  onSelectionChanged: (Set<bool> newSelection) {
+                    setState(() {
+                      _isMale = newSelection.first;
+                    });
+                  },
+                  showSelectedIcon: true,
+                  segments: const <ButtonSegment<bool>>[
+                    ButtonSegment<bool>(
+                        value: true, label: Text('ولد'), icon: Icon(Icons.boy)),
+                    ButtonSegment<bool>(
+                        value: false,
+                        label: Text('بنت'),
+                        icon: Icon(Icons.girl)),
+                  ],
+                  selected: <bool>{_isMale},
+                ),
+                const SizedBox(height: 24),
+                FilledButton(
+                  onPressed: onContinuePressed,
+                  child: const Text("اتفضل"),
+                )
+              ],
+            )),
+          ),
         ),
       ),
     );
